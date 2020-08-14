826b056980f3192fb29c53092b8d8ef07409eecf
Julia Chen <juliazychen@gmail.com>
2020-08-14 16:29:46 -0400

    Delete images.js

--- js/images.js
+++
@@ -1,9 +0,0 @@
-const images = document.getElementsByClassName("img");
-for (const element of images) {
-    var num = Math.floor((Math.random() * 16) + 1);
-    if (num <= 9) {
-        element.src = "images/pic0" + num + ".jpg";
-    } else {
-        element.src = "images/pic" + num + ".jpg";
-    }
-}
